27e2e99fdb6b2a7638aa3afcce3ef02ac64f7a71
eugenyk <uksan93@gmail.com>
2021-06-07 00:19:12 +0800

    css

--- src/js/components/todo-list/TodoList.jsx
+++ src/js/components/todo-list/TodoList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Todo from './todo/Todo';
+import s from '../../../css/modules/todo-list/TodoList.module.css'
 
 export default class TodoList extends React.Component {
     constructor(props) {
@@ -12,13 +13,8 @@ export default class TodoList extends React.Component {
     }
 
     render() {
-        const style = {
-            display: 'flex',
-            justifyContent: 'center'
-        }
-
         return (
-            <div style={style}>
+            <div className={s.todo_list}>
                 <div className="contaner">
                     {this.todos.map(todo => <Todo todo={todo} />)}
                 </div>
--- src/js/components/todo-list/todo/Todo.jsx
+++ src/js/components/todo-list/todo/Todo.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import s from '../../../../css/modules/todo-list/todo/Todo.module.css'
 
 export default class Todo extends React.Component {
     constructor(props) {
@@ -19,22 +20,12 @@ export default class Todo extends React.Component {
     }
 
     componentDidUpdate() {
-        console.log(this.state.hasDone);
+        console.log(s);
     }
 
     render() {
-        const todo = {
-            width: '290px',
-            height: '40px',
-
-            display: 'flex',
-            alignItems: 'center',
-            border: '1px solid black',
-            borderRadius: '5px',
-        }
-
         return (
-            <div style={todo}>
+            <div className={s.todo}>
                 <input type="checkbox" onChange={this.onClickCheckBox}/>
                 <p style={this.state.hasDone ? {color: 'red' } : {color: 'black'}}>{this.props.todo.text}</p>
             </div>
