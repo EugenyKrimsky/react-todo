5d2d0b46c85b279d28cd7b5dd2ec75961236cc47
eugeny <uksan93@gmail.com>
2021-08-24 23:09:47 +0300

    made setIsDone

--- src/components/js/TodoItem.jsx
+++ src/components/js/TodoItem.jsx
@@ -3,10 +3,12 @@ import c from '../css/TodoItem.module.css'
 
 const TodoItem = props => {
 
+    const [isDone, setIsDone] = useState(props.todo.isDone)
+
     return (
         <div className={c.TodoItem}>
             <p>{props.todo.text}</p>
-            <input type="checkbox"/>
+            <input type="checkbox" defaultChecked={isDone} onChange={() => setIsDone(!isDone)}/>
         </div>
     )
     
