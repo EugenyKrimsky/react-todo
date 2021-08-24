1867e5aa0257deed356ee18dfbf1f61212cbedee
eugeny <uksan93@gmail.com>
2021-08-24 22:43:39 +0300

    made adding new todo

--- src/components/js/NewTodoItem.jsx
+++ src/components/js/NewTodoItem.jsx
@@ -2,19 +2,22 @@ import React, { useState } from 'react'
 
 const NewTodoItem = ({addTodo}) => {
     
-    const [todo, setTodo] = useState('');
+    const [todoText, setTodoText] = useState('');
 
-    const onInput = value => {
-        setTodo(value);
+    const onInput = e => {
+        setTodoText(e.target.value);
     }
 
     const onAddTodo = () => {
-        addTodo(todo);
+        addTodo({
+            text: todoText,
+            isDone: false
+        });
     }
 
     return (
         <div>
-            <input type="text" onChange={() => onInput(this.value)}/>
+            <input type="text" onChange={onInput} value={todoText}/>
             <input type="button" value="Add" onClick={onAddTodo}/>
         </div>
     )
